13c1f2b1eec7b96d99fcf1186ccc415eda1cf36c
kawaguchi880 <yaoshengchuankou1@gmail.com>
2022-07-28 13:04:46 +0000

    add calculate.py

---
+++ calculate.py
@@ -0,0 +1,19 @@
+# 四則演算
+from random import sample
+
+
+print('<四則演算>')
+print(10+2)
+print(10-2)
+print(10*10)
+print(10 % 4)
+print('----------------------')
+# pythonでは配列はlistという
+sampleList = [10, 20, 30]
+print(type(sampleList))
+
+sampleDictionary = {}
+print(type(sampleDictionary))
+
+sampleTuple = ()
+print(type(sampleTuple))
